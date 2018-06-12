756558ae2275f56a83a7f32e8e09e2241ded63d4
Suzy <Suzy_Wu@asus.com>
2018-06-12 11:46:50 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+(function($) {
+    window.test= window.test || {};
+
+    $(function() {
+        test.initLandingPage();
+    });
+
+    test = {
+  		/* 一般初始化接口 */ 
+      init: function() {
+      }
+    };
+    window.test = test;
+   
+
+})(jQuery);
