6425039940b7fa1a71b462081803082001966a61
Suzy <Suzy_Wu@asus.com>
2018-06-14 14:15:32 +0800

    2nd modify js index

--- main.js
+++ main.js
@@ -9,7 +9,8 @@
   		/* 一般初始化接口 */ 
       init: function() {
           console.log('123')
-          console.log('123')
+          console.log('456')
+          console.log('789')
       }
     };
     window.test = test;
