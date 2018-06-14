647b3d77608c53cd114d36c99beee649fc633b92
Suzy <Suzy_Wu@asus.com>
2018-06-14 14:12:52 +0800

    modify js index

--- main.js
+++ main.js
@@ -8,7 +8,8 @@
     test = {
   		/* 一般初始化接口 */ 
       init: function() {
-      	console.log('123')
+          console.log('123')
+          console.log('123')
       }
     };
     window.test = test;
